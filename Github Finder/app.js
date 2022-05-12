4dc8b78f717127349a6158555490adf8b07b93b2
faisa <faisal.fahad.jed.k2@tuwaiq.edu.sa>
2022-05-12 10:10:20 +0300

    adding(not found)Alert && request UserRepo

--- Github Finder/app.js
+++ Github Finder/app.js
@@ -12,13 +12,15 @@ searchUser.addEventListener('keyup', (e) => {
    github.getUser(userText)
     .then(data => {
       if(data.profile.message === 'Not Found') {
-
+          ui.showAlert('User Not Found','alert alert-danger');
 
       } else {
           ui.showProfile(data.profile);
+          ui.showRepos(data.repos);
+
       }
     })
   } else {
-//
+ui.clearProfile();
   }
 }); 
--- Github Finder/github.js
+++ Github Finder/github.js
@@ -2,15 +2,21 @@ class Github {
   constructor() {
     this.client_id = '2be9374e1aa7a7964518';
     this.client_secret = '9359a286be71573ebef2674fc9f1d31ac316ab81';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
 
     return {
-      profile
+      profile,
+      repos
     }
-  }/****/
+  }
 }
